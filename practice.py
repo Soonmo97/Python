51a474680a4664ec67f72a614436946a5600419e
soonmo <smkwon97@gmail.com>
2024-12-29 12:26:34 +0900

    24/12/29 for

--- practice.py
+++ practice.py
@@ -513,3 +513,22 @@
 #     print("외투를 챙기세요")
 # else:
 #     print("너무 추워요. 나가지 마세요")
+
+
+# - for
+# print("대기번호 : 1")
+# print("대기번호 : 2")
+# print("대기번호 : 3")
+# print("대기번호 : 4")
+
+# for wating_no in [0, 1, 2, 3, 4]:
+#     print(f"대기번호 : {wating_no}")
+
+# for wating_no in range(1, 6): # 1, 2, 3, 4, 5
+#     print(f"대기번호 : {wating_no}")
+
+# starbucks = ["아이언맨", "토르", "아이엠 그루트"]
+# for customer in starbucks:
+#     print(f"{customer}, 커피가 준비되었습니다.")
+
+
