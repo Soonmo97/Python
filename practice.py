595bc8d338f8f663deabe2228232ac812b6f16a7
soonmo <smkwon97@gmail.com>
2024-12-30 20:19:48 +0900

    24/12/30 함수 - 전달값과 반환값

--- practice.py
+++ practice.py
@@ -616,7 +616,33 @@
 
 
 # - 함수
-def open_account():
-    print("새로운 계좌가 생성되었습니다.")
+# 선언 def 함수명():
+# def open_account():
+#     print("새로운 계좌가 생성되었습니다.")
 
-open_account()
+# open_account()
+
+# 전달값과 반환값
+# def deposit(balance, money): # 입금 balance: 잔액, money: 입금된 금액
+#     print(f"입금이 완료되었습니다. 잔액은 {balance + money} 원입니다.")
+#     return balance + money
+
+# def withdraw(balance, money) : # 출금
+#     if balance >= money: # 잔액이 출금액보다 같거나 많으면
+#         print(f"출금이 완료되었습니다. 잔액은 {balance - money} 원입니다.")
+#         return balance - money
+#     else: 
+#         print(f"출금이 완료되지 않았습니다. 잔액은 {balance} 원입니다.")
+#         return balance
+    
+# def withdraw_night(balance, money): # 저녁에 출금
+#     commission = 100 # 수수료 100원
+#     return commission, balance - money - commission # 여러개 값 반환 가능
+
+    
+# balance = 0 # 잔액
+# balance = deposit(balance, 1000)
+# # balance = withdraw(balance, 2000)
+# # balance = withdraw(balance, 500)
+# commission, balance = withdraw_night(balance, 500)
+# print(f"수수료 {commission} 원이며, 잔액은 {balance} 원입니다.")
