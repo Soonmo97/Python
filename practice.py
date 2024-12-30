fe4739bd12852b3a24cf6b22664fa13f9e01c130
soonmo <smkwon97@gmail.com>
2024-12-30 20:54:01 +0900

    24/12/30 함수 - 기본값

--- practice.py
+++ practice.py
@@ -622,7 +622,8 @@
 
 # open_account()
 
-# 전달값과 반환값
+
+# - 전달값과 반환값
 # def deposit(balance, money): # 입금 balance: 잔액, money: 입금된 금액
 #     print(f"입금이 완료되었습니다. 잔액은 {balance + money} 원입니다.")
 #     return balance + money
@@ -646,3 +647,21 @@
 # # balance = withdraw(balance, 500)
 # commission, balance = withdraw_night(balance, 500)
 # print(f"수수료 {commission} 원이며, 잔액은 {balance} 원입니다.")
+
+
+# - 기본값
+# def profile(name, age, main_lang): 
+#     print(f"이름: {name}\t나이: {age}\t주 사용 언어: {main_lang}")
+
+# profile("유재석", 20, "파이썬")
+# profile("김태호", 25, "자바")
+
+# 같은 학교 같은 학년 같은 반 같은 수업
+
+# def profile(name, age=17, main_lang="파이썬"): 
+#     print(f"이름: {name}\t나이: {age}\t주 사용 언어: {main_lang}")
+
+# profile("유재석")
+# profile("김태호")
+
+
