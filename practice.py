a7e4793a24793e8ae34c7644ba5e24ee97eda526
soonmo <smkwon97@gmail.com>
2025-01-03 06:03:01 +0900

    25/01/03 클래스

--- practice.py
+++ practice.py
@@ -900,3 +900,41 @@
 #         report_file.write("\n부서 :")
 #         report_file.write("\n이름 : ")
 #         report_file.write("\n업무 요약 :")
+
+
+# - 클래스
+# 마린 : 공격 유닛, 군인. 총을 쏠 수 있음
+# name = "마린" # 유닛의 이름
+# hp = 40 # 유닛의 체력
+# damage = 5 # 유닛의 공격력
+
+# print(f"{name} 유닛이 생성되었습니다.")
+# print(f"체력 {hp}, 공격력 {damage}\n")
+
+# # 탱크 : 공격 유닛, 탱크. 포를 쏠 수 있는데, 일반 모드 / 시즈 모드.
+# tank_name = "탱크"
+# tank_hp = 150
+# tank_damage = 36
+
+# print(f"{tank_name} 유닛이 생성되었습니다.")
+# print(f"체력 {tank_hp}, 공격력 {tank_damage}\n")
+
+# def attack(name, location, damage):
+#     print(f"{name} : {location} 방향으로 적군을 공격합니다. [공격력 {damage}]")
+
+# attack(name, "1시", damage)
+# attack(tank_name, "1시", tank_damage)
+# 위와 같은 방식으로 작성하면 유닛의 수가 많아질수록 코드가 무수히 길어지기에 클래스 필요
+
+# 클래스 생성
+# class Unit:
+#     def __init__ (self, name, hp, damage):
+#         self.name = name
+#         self.hp = hp
+#         self.damage = damage
+#         print(f"{self.name} 유닛이 생성되었습니다.")
+#         print(f"체력 {self.hp}, 공격력{self.damage}")
+
+# marine1 = Unit("마린", 40, 5)
+# marine2 = Unit("마린", 40, 5)
+# tank = Unit("탱크", 150, 35)
