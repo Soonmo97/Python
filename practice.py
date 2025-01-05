fd2d75ef13f18ceeea70e1d89c40451a76171442
soonmo <smkwon97@gmail.com>
2025-01-05 15:31:22 +0900

    25/01/05 클래스 - 멤버변수

--- practice.py
+++ practice.py
@@ -933,18 +933,29 @@
 # 인스턴스 변수 접근 - 클래스 내부에서 객체 고유의 데이터를 저장하거나 변경하려면 self를 통해 인스턴스 변수에 접근해야 한다.
 # 다른 메서드 호출 - 클래스 내부에서 다른 메서드를 호출할 때도 self를 사용해야 한다.
 
-# class Unit:
-#     def __init__ (self, name, hp, damage): 
-#         self.name = name
-#         self.hp = hp
-#         self.damage = damage
-#         print(f"{self.name} 유닛이 생성되었습니다.")
-#         print(f"체력 {self.hp}, 공격력{self.damage}")
+class Unit:
+    def __init__ (self, name, hp, damage): 
+        self.name = name
+        self.hp = hp
+        self.damage = damage
+        print(f"{self.name} 유닛이 생성되었습니다.")
+        print(f"체력 {self.hp}, 공격력{self.damage}")
 
 # marine1 = Unit("마린", 40, 5)
 # marine2 = Unit("마린", 40, 5)
 # tank = Unit("탱크", 150, 35)
 
 
+# - 멤버변수
+
+# 레이스: 공중 유닛, 비행기, 클로킹 (상대방에게 보이지 않음)
+wraith1 = Unit("레이스", 80, 5)
+print(f"유닛 이름: {wraith1.name}, 공격력: {wraith1.damage}")
+
+# 마인드 컨트롤: 상대방 유닛을 내 것으로 만드는 것 (빼앗음)
+wraith2 = Unit("빼앗긴 레이스", 80, 5)
+wraith2.clocking = True # 클래스 외부에서 변수 확장, 확장한 객체에만 적용 클래스 적용 x
+if wraith2.clocking == True:
+    print(f"{wraith2.name} 는 클로킹 상태입니다.")
 
 
