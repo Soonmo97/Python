9c545ca9eea98051b671d5d6dcf4bf917f4c16eb
soonmo <smkwon97@gmail.com>
2025-01-05 15:43:41 +0900

    25/01/05 클래스 - 메소드

--- practice.py
+++ practice.py
@@ -949,13 +949,41 @@ class Unit:
 # - 멤버변수
 
 # 레이스: 공중 유닛, 비행기, 클로킹 (상대방에게 보이지 않음)
-wraith1 = Unit("레이스", 80, 5)
-print(f"유닛 이름: {wraith1.name}, 공격력: {wraith1.damage}")
+# wraith1 = Unit("레이스", 80, 5)
+# print(f"유닛 이름: {wraith1.name}, 공격력: {wraith1.damage}")
 
-# 마인드 컨트롤: 상대방 유닛을 내 것으로 만드는 것 (빼앗음)
-wraith2 = Unit("빼앗긴 레이스", 80, 5)
-wraith2.clocking = True # 클래스 외부에서 변수 확장, 확장한 객체에만 적용 클래스 적용 x
-if wraith2.clocking == True:
-    print(f"{wraith2.name} 는 클로킹 상태입니다.")
+# # 마인드 컨트롤: 상대방 유닛을 내 것으로 만드는 것 (빼앗음)
+# wraith2 = Unit("빼앗긴 레이스", 80, 5)
+# wraith2.clocking = True # 클래스 외부에서 변수 확장, 확장한 객체에만 적용 클래스 적용 x
+# if wraith2.clocking == True:
+#     print(f"{wraith2.name} 는 클로킹 상태입니다.")
+
+
+# - 메소드
+
+# 공격 유닛
+# class AttackUnit:
+#     def __init__ (self, name, hp, damage): 
+#         self.name = name
+#         self.hp = hp
+#         self.damage = damage
+    
+#     def attack(self, location):
+#         print(f"{self.name} : {location} 방향으로 적군을 공격합니다. [공격력 {self.damage}]")
+
+#     def damaged(self, damage):
+#         print(f"{self.name} : {damage} 데미지를 입었습니다.")
+#         self.hp -= damage
+#         print(f"{self.name} : 현재 체력은 {self.hp} 입니다.")
+#         if self.hp <= 0:
+#             print(f"{self.name} : 파괴되었습니다.")
+
+# # 파이어뱃: 공격 유닛, 화염방사기.
+# firebat1 = AttackUnit("파이어뱃", 50, 16)
+# firebat1.attack("5시")
+
+# # 공격 2번 받는다고 가정
+# firebat1.damaged(25)
+# firebat1.damaged(25)
 
 
