2e66f1fc56fd690b4d431a27fbd1d35b37111cf6
soonmo <smkwon97@gmail.com>
2025-01-17 13:57:09 +0900

    25/01/17 클래스 - 다중상속

--- practice.py
+++ practice.py
@@ -959,40 +959,47 @@
 #     print(f"{wraith2.name} 는 클로킹 상태입니다.")
 
 
-# - 메소드, 상속
+# - 메소드, 상속, 다중상속
 
 # 일반 유닛
-# class Unit:
-#     def __init__ (self, name, hp): 
-#         self.name = name
-#         self.hp = hp
+class Unit:
+    def __init__ (self, name, hp): 
+        self.name = name
+        self.hp = hp
 
 # # 공격 유닛
-# class AttackUnit(Unit):
-#     def __init__ (self, name, hp, damage): 
-#         Unit.__init__(self, name, hp)
-#         self.damage = damage
+class AttackUnit(Unit):
+    def __init__ (self, name, hp, damage): 
+        Unit.__init__(self, name, hp)
+        self.damage = damage
     
-#     def attack(self, location):
-#         print(f"{self.name} : {location} 방향으로 적군을 공격합니다. [공격력 {self.damage}]")
+    def attack(self, location):
+        print(f"{self.name} : {location} 방향으로 적군을 공격합니다. [공격력 {self.damage}]")
 
-#     def damaged(self, damage):
-#         print(f"{self.name} : {damage} 데미지를 입었습니다.")
-#         self.hp -= damage
-#         print(f"{self.name} : 현재 체력은 {self.hp} 입니다.")
-#         if self.hp <= 0:
-#             print(f"{self.name} : 파괴되었습니다.")
+    def damaged(self, damage):
+        print(f"{self.name} : {damage} 데미지를 입었습니다.")
+        self.hp -= damage
+        print(f"{self.name} : 현재 체력은 {self.hp} 입니다.")
+        if self.hp <= 0:
+            print(f"{self.name} : 파괴되었습니다.")
 
-# # # 파이어뱃: 공격 유닛, 화염방사기.
-# firebat1 = AttackUnit("파이어뱃", 50, 16)
-# firebat1.attack("5시")
+# 드랍쉽 : 공중 유닛, 수송기. 마린 / 파이어뱃 / 탱크 등을 수송. 공격 X
 
-# # # 공격 2번 받는다고 가정
-# firebat1.damaged(25)
-# firebat1.damaged(25)
+# 날 수 있는 기능을 가진 클래스
+class Flyable:
+    def __init__(self, flying_speed):
+        self.flying_speed = flying_speed
 
+    def fly(self, name, location):
+        print(f"{name} : {location} 방향으로 날아갑니다. [속도 {self.flying_speed}]")
 
+# 공중 공격 유닛 클래스
+class FlyableAttackUnit(AttackUnit, Flyable):
+    def __init__(self, name, hp, damage, flying_speed):
+        AttackUnit.__init__(self, name, hp, damage)
+        Flyable.__init__(self, flying_speed)
 
-
-
+# 발키리 : 공중 공격 유닛, 한 번에 14발 미사일 발사.
+valkyrie = FlyableAttackUnit("발키리", 200, 6, 5)
+valkyrie.fly(valkyrie.name, "3시")
 
